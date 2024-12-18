384b3d585a5705993d28c5a357f04f3e7588576c
LeeTrent <lee.trent.1@gmail.com>
2024-12-17 21:43:22 -0600

    Added extension methods for catalog, basket and ordering

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,4 +1,14 @@
+using Basket;
+using Catalog;
+using Ordering;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddCatalogModule(builder.Configuration)
+    .AddBasketModule(builder.Configuration)
+    .AddOrderingModule(builder.Configuration);
+
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
