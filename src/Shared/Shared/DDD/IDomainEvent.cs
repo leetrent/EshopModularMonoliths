1e2ec7337b369d0ef5e6cb3e6ae7d2f20294a91f
LeeTrent <lee.trent.1@gmail.com>
2024-12-19 21:09:30 -0600

    Added DDD entities

---
+++ src/Shared/Shared/DDD/IDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Shared.DDD
+{
+    public interface IDomainEvent : INotification
+    {
+        Guid EventId => Guid.NewGuid();
+        public DateTime OccurredIn => DateTime.Now;
+        public string EventType => GetType().AssemblyQualifiedName!;
+    }
+}
