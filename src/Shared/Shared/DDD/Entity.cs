54be0d38996b30faa752807e0c18da05bcdf30ee
LeeTrent <lee.trent.1@gmail.com>
2024-12-21 10:08:12 -0600

    Added Product domain entity

--- src/Shared/Shared/DDD/Entity.cs
+++ src/Shared/Shared/DDD/Entity.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shared.DDD
+﻿namespace Shared.DDD
 {
-    public abstract class Entity<T> :IEntity<T>
+    public abstract class Entity<T> : IEntity<T>
     {
-        public T Id { get; set; }
+        public required T Id { get; set; }
         public DateTime? CreatedAt { get; set; }
         public string? CreatedBy { get; set; }
         public DateTime? LastModified { get; set; }
