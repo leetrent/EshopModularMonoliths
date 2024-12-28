52ffff396565b9dfdffea6900f450838d701c5a7
LeeTrent <lee.trent.1@gmail.com>
2024-12-27 21:20:13 -0600

    Created catalogue DB context

--- src/Modules/Catalog/Catalog/GlobalUsing.cs
+++ src/Modules/Catalog/Catalog/GlobalUsing.cs
@@ -1,3 +1,6 @@
 ﻿global using Shared.DDD;
 global using Catalog.Products.Models;
 global using Catalog.Products.Events;
+global using System.Reflection;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
