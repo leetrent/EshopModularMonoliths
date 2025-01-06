f0a4f8a5cd4deb48b6b365e3419f00fe90cb52b3
LeeTrent <lee.trent.1@gmail.com>
2025-01-05 22:49:47 -0600

    Now seeding catalog data

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -11,16 +11,13 @@ namespace Catalog
 
             var connectionString = configuration.GetConnectionString("Database");
             services.AddDbContext<CatalogDbContext>(options => options.UseNpgsql(connectionString));
-            
+            services.AddScoped<IDataSeeder, CatalogDataSeeder>();
             return services;
         }
 
         public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
         {
-            //app
-            //    .UseApplicationServices()
-            //    .UseInfrastructureServices()
-            //    .UseApiServices();
+            app.UseMigration<CatalogDbContext>();
             return app;
         }
     }
---
+++ src/Modules/Catalog/Catalog/Data/Seed/CatalogDataSeeder.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Shared.Data.Seed;
+
+namespace Catalog.Data.Seed
+{
+    public class CatalogDataSeeder(CatalogDbContext dbContext) 
+        : IDataSeeder
+    {
+        public async Task SeedAllAsync()
+        {
+            if (await dbContext.Products.AnyAsync() == false)
+            {
+                await dbContext.Products.AddRangeAsync(InitialData.Products);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
