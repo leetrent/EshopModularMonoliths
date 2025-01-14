057863aaca83274733ef216dcb6a0b03b80db125
LeeTrent <lee.trent.1@gmail.com>
2025-01-14 06:57:45 -0600

    Implemented the create product use case

--- src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,30 @@
-﻿namespace Catalog.Products.Features.CreateProduct
+﻿namespace Catalog.Products.Features.CreateProduct;
+
+public record CreateProductResult(Guid Id);
+public record CreateProductCommand(ProductDto Product)
+    : ICommand<CreateProductResult>;
+
+public class CreateProductCommandHandler(CatalogDbContext dbContext)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public class CreateProductHandler
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        Product product = CreateNewProduct(command.Product);
+        dbContext.Products.Add(product);
+        await dbContext.SaveChangesAsync(cancellationToken);
+        return new CreateProductResult(product.Id);
+    }
+
+    private Product CreateNewProduct(ProductDto dto)
+    {
+        return Product.Create
+        (
+            Guid.NewGuid(),
+            dto.Name,
+            dto.Category,
+            dto.Description,
+            dto.ImageFile,
+            dto.Price
+        );
     }
 }
