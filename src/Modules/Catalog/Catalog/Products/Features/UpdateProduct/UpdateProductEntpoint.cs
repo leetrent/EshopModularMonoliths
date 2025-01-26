6680357c87e4838ee37dd54c0e986327c7493268
LeeTrent <lee.trent.1@gmail.com>
2025-01-26 09:31:23 -0600

    Developed update product API endpoint

---
+++ src/Modules/Catalog/Catalog/Products/Features/UpdateProduct/UpdateProductEntpoint.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Catalog.Products.Features.UpdateProduct
+{
+    public record UpdateProductRequest(ProductDto Product);
+    public record UpdateProductResponse(bool IsSuccess);
+    public class UpdateProductEntpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+            {
+                UpdateProductCommand command = request.Adapt<UpdateProductCommand>();
+                UpdateProductResult result = await sender.Send(command);
+                UpdateProductResponse response = result.Adapt<UpdateProductResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("UpdateProduct")
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Update Product")
+            .WithDescription("Update Prdouct");
+        }
+    }
+}
