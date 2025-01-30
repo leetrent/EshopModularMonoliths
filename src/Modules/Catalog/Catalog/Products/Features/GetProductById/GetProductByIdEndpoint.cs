76586f377d41075a04fc5a4eb6a636cc75caed4a
LeeTrent <lee.trent.1@gmail.com>
2025-01-29 20:42:21 -0600

    Added GetProductByIdEndpoint class

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.Products.Features.GetProductById
+{  public record GetProductByIdResponse(ProductDto Product);
+
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                GetProductByIdResult result = await sender.Send(new GetProductByIdQuery(id));
+                GetProductByIdResponse response = result.Adapt<GetProductByIdResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProductBtId")
+            .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product By Id")
+            .WithDescription("Get Product By Id");
+        }
+    }
+}
