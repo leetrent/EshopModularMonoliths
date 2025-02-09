51383ec5fe6c554035ac0571e0f39a9015b2de7c
LeeTrent <lee.trent.1@gmail.com>
2025-02-09 11:01:11 -0600

    Added Create product validation using Fluent validation library

--- src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
@@ -1,8 +1,20 @@
 ﻿namespace Catalog.Products.Features.CreateProduct;
 
 public record CreateProductResult(Guid Id);
+
 public record CreateProductCommand(ProductDto Product)
     : ICommand<CreateProductResult>;
+
+public class CreateProductCommandValidator: AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Product.Name).NotEmpty().WithMessage("Product name is required");
+        RuleFor(x => x.Product.Category).NotEmpty().WithMessage("Product category is required");
+        RuleFor(x => x.Product.ImageFile).NotEmpty().WithMessage("Product image file is required");
+        RuleFor(x => x.Product.Price).GreaterThan(0).WithMessage("Product price must be greater than 0");
+    }
+}
 
 public class CreateProductHandler(CatalogDbContext dbContext)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
