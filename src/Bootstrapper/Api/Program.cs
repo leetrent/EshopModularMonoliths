ff834d0b7364f25e5766140ab7260ba2eec6fafe
LeeTrent <lee.trent.1@gmail.com>
2025-02-17 21:56:13 -0600

    Added custom exception handling

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,3 +1,10 @@
+//using System.Reflection;
+//using Microsoft.AspNetCore.Diagnostics;
+//using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddCarterWithAssemblies(typeof(CatalogModule).Assembly);
@@ -11,4 +18,33 @@ app.MapCarter();
 app.UseCatalogModule();
 app.UseBasketModule();
 app.UseOrderingModule();
+
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception == null)
+        {
+            return;
+        }
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
+
+
 app.Run();
